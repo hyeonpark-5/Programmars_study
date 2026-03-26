86b93c94887fa5669e8c35ba976b8f7802f9ba23
hyeonpark-5 <sorkgus@gmail.com>
2026-03-26 14:32:05 +0900

    n7

---
+++ LV3/n7.py
@@ -0,0 +1,12 @@
+# 단속카메라
+def solution(routes):
+    answer = 0
+    routes.sort(key=lambda x:x[1])
+    
+    camera = -300001
+    for route in routes:
+        if route[0] > camera:
+            answer += 1
+            camera = route[1]
+
+    return answer
